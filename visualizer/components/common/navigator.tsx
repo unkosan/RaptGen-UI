638622ae8cb5173ad269f32d52051bdb32567153
Ryota Nakano <71750787+unkosan@users.noreply.github.com>
2023-04-09 04:12:17 +0900

    temp

---
+++ visualizer/components/common/navigator.tsx
@@ -0,0 +1,22 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import Link from "next/link";
+
+const Navigator: React.FC = () => {
+    return (
+        <Navbar bg="primary" variant="dark">
+            <Container>
+                <Navbar.Brand>
+                    RaptGen Visualizer
+                </Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/viewer">
+                        Viewer
+                    </Nav.Link>
+                    <Nav.Link href="/uploader">
+                        Uploader
+                    </Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
