09d2cfae41413abc5cc51e7667f341f9dfb199ee
Ryota Nakano <71750787+unkosan@users.noreply.github.com>
2023-05-25 21:41:19 +0900

    applied reactdatagrid to DataTable component

--- visualizer/components/uploader/sidebar-vae/data-table/data-table.tsx
+++ visualizer/components/uploader/sidebar-vae/data-table/data-table.tsx
@@ -1,8 +1,60 @@
 import { Table } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
+import ReactDataGrid from "@inovua/reactdatagrid-community";
+import "@inovua/reactdatagrid-community/index.css";
+import { useCallback } from "react";
+
+const columns = [
+  { name: "id", type: "number", header: "ID", defaultVisible: false },
+  { name: "trimmedSequences", header: "Trimmed Sequences", defaultFlex: 1 },
+  { name: "duplicates", header: "Duplicates" },
+];
+
+const gridStyle = { minHeight: 550, width: "100%" };
 
 const DataTable: React.FC = () => {
+  const sequenceData = useSelector(
+    (state: RootState) => state.vaeConfig.sequenceData
+  );
+  const data = sequenceData.randomRegions
+    .map((seq, i) => {
+      return seq
+        ? {
+            id: i,
+            trimmedSequences: seq,
+            duplicates: sequenceData.duplicates[i],
+          }
+        : null;
+    })
+    .filter((obj) => obj !== null);
+  const onCopyActiveRowChange = useCallback((row: unknown) => {
+    console.log(row);
+  }, []);
+  return (
+    <div style={{ zIndex: 1000 }}>
+      <ReactDataGrid
+        idProperty="id"
+        style={gridStyle}
+        columns={columns}
+        dataSource={data}
+        rowHeight={25}
+        pagination
+        defaultLimit={20}
+        enableSelection
+        multiSelect
+        enableClipboard
+        onCopyActiveRowChange={onCopyActiveRowChange}
+        rowStyle={{
+          fontFamily: "monospace",
+          wrap: "break-word",
+        }}
+      />
+    </div>
+  );
+};
+
+const DataTable2: React.FC = () => {
   const sequenceData = useSelector(
     (state: RootState) => state.vaeConfig.sequenceData
   );
