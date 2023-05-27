573a5f5fbd8c2e29a5184e46ac29bdb8c72cabb6
Ryota Nakano <71750787+unkosan@users.noreply.github.com>
2023-05-28 00:13:29 +0900

    changed decode seqtable to use reactdatagrid

--- visualizer/components/viewer/operator-control/decode/decode-table.tsx
+++ visualizer/components/viewer/operator-control/decode/decode-table.tsx
@@ -5,6 +5,7 @@ import {
   Eraser,
   Check2,
   X,
+  Trash,
 } from "react-bootstrap-icons";
 import { DecodeDataEntry } from "../../redux/decode-data";
 import React, { useEffect, useState } from "react";
@@ -13,19 +14,21 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import axios from "axios";
 import { Button, ButtonGroup, Form, Table } from "react-bootstrap";
+import ReactDataGrid from "@inovua/reactdatagrid-community";
 
-type RecordProps = {
-  record: DecodeDataEntry;
+type CoordEditorProps = {
+  value: number;
+  onComplete: () => void;
+  onCancel: () => void;
+  onChange: (value: number) => void;
+  onKeyDown: (e: React.KeyboardEvent) => void;
+  cellProps: any;
 };
 
-const Record: React.FC<RecordProps> = React.memo<RecordProps>(function _Record(
-  props
-) {
-  const [isEditing, setIsEditing] = useState<boolean>(false);
-  const [valueX, setValueX] = useState<number>(props.record.coordX);
-  const [valueY, setValueY] = useState<number>(props.record.coordY);
-  const [isDirty, setIsDirty] = useState<boolean>(false);
-  const [isValid, setIsValid] = useState<boolean>(true);
+const CoordEditor: React.FC<CoordEditorProps> = (props) => {
+  const [valueX, setValueX] = useState<string>(props.cellProps.data.coordX);
+  const [valueY, setValueY] = useState<string>(props.cellProps.data.coordY);
+  const [valid, setValid] = useState<boolean>(true);
 
   const dispatch = useDispatch();
   const decodeData = useSelector((state: RootState) => state.decodeData);
@@ -34,199 +37,272 @@ const Record: React.FC<RecordProps> = React.memo<RecordProps>(function _Record(
     (state: RootState) => state.sessionConfig.sessionId
   );
 
+  const onChangeX: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValueX(e.target.value);
+  };
+
+  const onChangeY: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValueY(e.target.value);
+  };
+
   useEffect(() => {
-    const xValid = !isNaN(valueX);
-    const yValid = !isNaN(valueY);
-    setIsValid(xValid && yValid);
+    const xValid = !isNaN(parseFloat(valueX));
+    const yValid = !isNaN(parseFloat(valueY));
+    setValid(xValid && yValid);
   }, [valueX, valueY]);
 
-  const onChangeShow = () => {
-    // dispatch action to change show status
-    const idx = decodeData.findIndex((e) => e.key === props.record.key);
-    let newDecodeData = [...decodeData];
-    let newRecord: DecodeDataEntry = { ...props.record };
-    newRecord.isShown = !newRecord.isShown;
-    newDecodeData[idx] = newRecord;
-    dispatch({
-      type: "decodeData/set",
-      payload: newDecodeData,
-    });
-  };
+  const onConfirmClick = async () => {
+    const key: number = props.cellProps.data.key;
+    const idx = decodeData.findIndex((e) => e.key === key);
+    const newDecodeData = [...decodeData];
 
-  const onEdit = () => {
-    setIsEditing(true);
-  };
-
-  const onRemove = () => {
-    // dispatch action to remove record
-    const idx = decodeData.findIndex((e) => e.key === props.record.key);
-    let newDecodeData = [...decodeData];
-    newDecodeData.splice(idx, 1);
-    dispatch({
-      type: "decodeData/set",
-      payload: newDecodeData,
-    });
-  };
-
-  const onEditCancel = () => {
-    setIsEditing(false);
-    setIsDirty(false);
-    setValueX(props.record.coordX);
-    setValueY(props.record.coordY);
-  };
-
-  const onEditSave = async () => {
-    if (isValid && isDirty) {
-      // dispatch action to update record
-      const idx = decodeData.findIndex((e) => e.key === props.record.key);
-      let newDecodeData = [...decodeData];
-      let newRecord: DecodeDataEntry = {
-        ...props.record,
-      };
-      newRecord.coordX = valueX;
-      newRecord.coordY = valueY;
-
-      const res = await axios
-        .post("/session/decode", {
-          session_id: sessionId,
-          coords: [
-            {
-              coord_x: valueX,
-              coord_y: valueY,
-            },
-          ],
-        })
-        .then((res) => res.data);
-
-      if (res.status !== "success") {
-        return;
-      }
-
-      const sequence = res.data[0];
-      newRecord.randomRegion = sequence;
-
-      newDecodeData[idx] = newRecord;
-      dispatch({
-        type: "decodeData/set",
-        payload: newDecodeData,
+    const res = await axios
+      .post("/session/decode", {
+        session_id: sessionId,
+        coords: [
+          {
+            coord_x: parseFloat(valueX),
+            coord_y: parseFloat(valueY),
+          },
+        ],
+      })
+      .then((res) => res.data)
+      .catch((err) => {
+        console.log(err);
+        return [];
       });
-      setIsEditing(false);
-    }
+    console.log("res", res);
+    newDecodeData[idx] = {
+      ...newDecodeData[idx],
+      coordX: parseFloat(valueX),
+      coordY: parseFloat(valueY),
+      randomRegion: res.data[0],
+      sequence: "",
+    };
+
+    dispatch({
+      type: "decodeData/set",
+      payload: newDecodeData,
+    });
+
+    props.onComplete();
   };
 
-  const onChangeX = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValueX(parseFloat(e.target.value));
-    setIsDirty(true);
-  };
-  const onChangeY = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValueY(parseFloat(e.target.value));
-    setIsDirty(true);
-  };
-
-  if (isEditing) {
-    return (
-      <tr key={props.record.key}>
-        <td>
-          <Form.Control
-            type="number"
-            value={valueX}
-            onChange={onChangeX}
-            step={0.1}
-          />
-        </td>
-        <td>
-          <Form.Control
-            type="number"
-            value={valueY}
-            onChange={onChangeY}
-            step={0.1}
-          />
-        </td>
-        <td className="font-monospace text-break">
-          {props.record.randomRegion}
-        </td>
-        <td>
-          <ButtonGroup>
-            <Button
-              variant="success"
-              onClick={onEditSave}
-              disabled={!isValid || !isDirty}
-            >
-              <Check2 />
-            </Button>
-            <Button
-              variant="danger"
-              className="px-2 py-1"
-              onClick={onEditCancel}
-            >
-              <X />
-            </Button>
-          </ButtonGroup>
-        </td>
-      </tr>
-    );
-  } else {
-    return (
-      <tr key={props.record.key}>
-        <td>{props.record.coordX}</td>
-        <td>{props.record.coordY}</td>
-        <td className="font-monospace text-break">
-          {props.record.randomRegion}
-        </td>
-        <td>
-          <ButtonGroup>
-            {props.record.isShown ? (
-              <Button
-                variant="primary"
-                className="px-1 py-0"
-                onClick={onChangeShow}
-              >
-                <Eye />
-              </Button>
-            ) : (
-              <Button
-                variant="outline-primary"
-                className="px-1 py-0"
-                onClick={onChangeShow}
-              >
-                <EyeSlash />
-              </Button>
-            )}
-            <Button variant="success" className="px-1 py-0" onClick={onEdit}>
-              <PencilSquare />
-            </Button>
-            <Button variant="danger" className="px-1 py-0" onClick={onRemove}>
-              <Eraser />
-            </Button>
-          </ButtonGroup>
-        </td>
-      </tr>
-    );
-  }
-});
-
-const DecodeTable: React.FC = React.memo(function _Table() {
-  const decodeData = useSelector((state: RootState) => state.decodeData);
+  const style = Object.assign(
+    {
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      background: "white",
+      color: "inherit",
+      alignItems: "center",
+      position: "absolute",
+      justifyContent: "space-between",
+      left: 0,
+      top: 0,
+    },
+    valid
+      ? {}
+      : {
+          borderColor: "rgba(255, 0, 0, 0.5)",
+          boxShadow: "0 0 0 2px rgba(255, 0, 0, 0.2)",
+        }
+  ) as React.CSSProperties;
 
   return (
-    <div style={{ height: "200px", overflowY: "auto" }}>
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>X</th>
-            <th>Y</th>
-            <th>Sequence</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {decodeData.slice(1).map((record) => (
-            <Record key={record.key} record={record} />
-          ))}
-        </tbody>
-      </Table>
+    <div
+      style={style}
+      className="inovua-react-toolkit-text-input InovuaReactDataGrid__cell__editor InovuaReactDataGrid__cell__editor--text  inovua-react-toolkit-text-input--ltr inovua-react-toolkit-text-input--theme-default-light inovua-react-toolkit-text-input--enable-clear-button inovua-react-toolkit-text-input--focused"
+    >
+      <div style={{ flex: 1, display: "flex" }}>
+        &#40;
+        <input
+          value={valueX}
+          onChange={onChangeX}
+          style={{
+            width: 0,
+            flexShrink: 1,
+            flexGrow: 1,
+            border: "none",
+            background: "rgba(0, 0, 0, 0.1)",
+            color: "inherit",
+            outline: "none",
+            padding: "0 0.5rem",
+          }}
+        />
+        ,
+        <input
+          value={valueY}
+          onChange={onChangeY}
+          style={{
+            width: 0,
+            flexShrink: 1,
+            flexGrow: 1,
+            border: "none",
+            background: "rgba(0, 0, 0, 0.1)",
+            color: "inherit",
+            outline: "none",
+            padding: "0 0.5rem",
+          }}
+        />
+        &#41;
+      </div>
+
+      <Check2
+        size={18}
+        style={{
+          cursor: "pointer",
+          marginInline: "0.2rem",
+          color: valid ? "grey" : "lightgrey",
+        }}
+        onClick={valid ? onConfirmClick : () => {}}
+      />
+      <X
+        size={20}
+        style={{ cursor: "pointer", marginInline: "0.2rem", color: "grey" }}
+        onClick={() => {
+          props.onCancel();
+        }}
+      />
     </div>
   );
-});
+};
+
+type ActionProps = {
+  data: {
+    key: number;
+    id: string;
+    coordX: number;
+    coordY: number;
+    randomRegion: string;
+    isShown: boolean;
+  };
+};
+
+const Actions: React.FC<ActionProps> = (props) => {
+  const { data } = props;
+  const key = data.key;
+  const dispatch = useDispatch();
+  const decodeData = useSelector((state: RootState) => state.decodeData);
+
+  const onClickShow = async () => {
+    const idx = decodeData.findIndex((e) => e.key === key);
+    const newDecodeData = [...decodeData];
+    newDecodeData[idx] = {
+      ...newDecodeData[idx],
+      isShown: !data.isShown,
+    };
+
+    dispatch({
+      type: "decodeData/set",
+      payload: newDecodeData,
+    });
+  };
+
+  const onClickDelete = async () => {
+    const idx = decodeData.findIndex((e) => e.key === key);
+    const newDecodeData = [...decodeData];
+    newDecodeData.splice(idx, 1);
+
+    dispatch({
+      type: "decodeData/set",
+      payload: newDecodeData,
+    });
+  };
+
+  const showStyle = {
+    cursor: "pointer",
+    borderRadius: 4,
+    height: "24px",
+    width: "24px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: data.isShown ? "#e8e8e8" : "inherit",
+    background: data.isShown ? "#7986cb" : "#ffffff",
+    border: "2px solid #7986cb",
+    marginInline: "0.2rem",
+  } as React.CSSProperties;
+
+  const deleteStyle = {
+    cursor: "pointer",
+    borderRadius: 4,
+    height: "24px",
+    width: "24px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#ffffff",
+    background: "#ff6347",
+    border: "2px solid #ff6347",
+    marginInline: "0.2rem",
+  } as React.CSSProperties;
+
+  return (
+    <div className="d-flex">
+      <span style={showStyle} onClick={onClickShow}>
+        {data.isShown ? <Eye size={16} /> : <EyeSlash size={16} />}
+      </span>
+      <span style={deleteStyle} onClick={onClickDelete}>
+        <Trash size={16} />
+      </span>
+    </div>
+  );
+};
+
+const columns = [
+  { name: "key", header: "Key", defaultVisible: false, editable: false },
+  { name: "id", header: "ID", defaultVisible: false, editable: false },
+  {
+    name: "coord",
+    header: "Coord",
+    width: 160,
+    editable: true,
+    renderEditor: (props: CoordEditorProps) => <CoordEditor {...props} />,
+  },
+  {
+    name: "randomRegion",
+    header: "Random Region",
+    defaultFlex: 1,
+    editable: false,
+  },
+  {
+    name: "actions",
+    header: "Actions",
+    width: 100,
+    defaultVisible: false,
+    editable: false,
+    render: (props: ActionProps) => <Actions {...props} />,
+  },
+];
+
+const gridStyle = { minHeight: 400, width: "100%", zIndex: 1000 };
+
+const DecodeTable: React.FC = () => {
+  const decodeData = useSelector((state: RootState) => state.decodeData);
+  const data = decodeData.slice(1).map((e) => ({
+    key: e.key,
+    id: e.id,
+    coordX: e.coordX,
+    coordY: e.coordY,
+    randomRegion: e.randomRegion,
+    isShown: e.isShown,
+    coord: "(" + e.coordX.toFixed(3) + ", " + e.coordY.toFixed(3) + ")",
+  }));
+
+  return (
+    <ReactDataGrid
+      idProperty="key"
+      columns={columns}
+      dataSource={data}
+      editable={true}
+      rowStyle={{ fontFamily: "monospace" }}
+      pagination
+      defaultLimit={20}
+      rowHeight={35}
+      style={gridStyle}
+    />
+  );
+};
 
 export default DecodeTable;
--- visualizer/components/viewer/operator-control/decode/decode.tsx
+++ visualizer/components/viewer/operator-control/decode/decode.tsx
@@ -2,6 +2,7 @@ import { Form } from "react-bootstrap";
 import PointDecoder from "./point-decoder";
 import ResultViewer from "./result-viewer";
 import DecodeTable from "./decode-table";
+import ClientOnly from "../../../common/client-only";
 
 const Decode: React.FC = () => {
   return (
@@ -12,7 +13,9 @@ const Decode: React.FC = () => {
         <Form.Label>Decoded Sequence</Form.Label>
         <ResultViewer />
         <Form.Label>Sequence List</Form.Label>
-        <DecodeTable />
+        <ClientOnly>
+          <DecodeTable />
+        </ClientOnly>
       </Form.Group>
     </Form>
   );
