da74932951099c7be9d4916e6e70a6987e0bfce1
Ryota Nakano <71750787+unkosan@users.noreply.github.com>
2023-06-11 23:20:38 +0900

    argumented store data to redux

--- visualizer/components/trainer/add/redux/page-config.ts
+++ visualizer/components/trainer/add/redux/page-config.ts
@@ -2,18 +2,26 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 type PageConfig = {
   pseudoRoute: string; // "/selex" or "/train"
+  modelType: string; // needed for future implementation of multiple model types (layout could change)
 };
 
 const pageConfigSlice = createSlice({
   name: "pageConfig",
   initialState: {
     pseudoRoute: "/selex",
+    modelType: "RaptGen",
   },
   reducers: {
     setRoute: (state: PageConfig, action: PayloadAction<string>) => {
       return {
         ...state,
         pseudoRoute: action.payload,
+      };
+    },
+    setModelType: (state: PageConfig, action: PayloadAction<string>) => {
+      return {
+        ...state,
+        modelType: action.payload,
       };
     },
   },
