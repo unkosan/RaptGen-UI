b9fc45f7ffcf0698f771be40698f81a0abe9e884
Ryota Nakano <71750787+unkosan@users.noreply.github.com>
2023-08-23 20:45:07 +0900

    added session handlers

--- visualizer/mock/handlers.ts
+++ visualizer/mock/handlers.ts
@@ -1,8 +1,9 @@
 import { rest } from "msw";
 import { dataHandlers } from "./route/data";
+import { sessionHandlers } from "./route/session";
 
 export const mockURL = (path: string) => {
   return `http://localhost:8000/api${path}`;
 };
 
-export const handlers = [...dataHandlers];
+export const handlers = [...dataHandlers, ...sessionHandlers];
