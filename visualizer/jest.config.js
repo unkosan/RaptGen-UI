71c01e5217d80fd1b57e962b5715777a5e8fc36b
Ryota Nakano <71750787+unkosan@users.noreply.github.com>
2023-09-05 18:05:04 +0900

    Fix: jest did not work for jsx

--- visualizer/jest.config.js
+++ visualizer/jest.config.js
@@ -1,5 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
+  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(test).ts?(x)"],
+  transform: {
+    "^.+\\.tsx$": [
+      "ts-jest",
+      {
+        babel: true,
+        tsconfig: "tsconfig.test.json",
+      },
+    ],
+  },
 };
