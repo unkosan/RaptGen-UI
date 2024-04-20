12fde5cfa9914b410bfa9a1536f746b340f5c2bf
Ryota Nakano <71750787+unkosan@users.noreply.github.com>
2024-04-20 16:01:53 +0900

    feat: added JobDatabaseService

--- raptgen/core/psqldb.py
+++ raptgen/core/psqldb.py
@@ -1,9 +1,11 @@
 from typing import Literal, Optional, List, Any, Dict
+import numpy as np
 
-from raptgen.tasks import celery
+from tasks import celery
 
-from sqlalchemy.orm import sessionmaker, relationship, declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base, Session
 from sqlalchemy.sql import func
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import (
     Column,
     Integer,
@@ -17,15 +19,9 @@ from sqlalchemy import (
 )
 
 # create a postgres engine and its session
-engine = create_engine(
-    "postgresql://postgres:postgres@db:5432"
-)
-session = sessionmaker(bind=engine)()
-
-
 # define schemas for the database
 BaseSchema = declarative_base()
-BaseSchema.metadata.create_all(engine) 
+
 
 class ParentJob(BaseSchema):
     """
@@ -72,10 +68,10 @@ class ParentJob(BaseSchema):
 class ChildJob(BaseSchema):
     """
     Table of meta-info on Child-Jobs
-    
+
     Attributes
     ----------
-    
+
     id : int
         zero-indexed identifier, represents the order of the child jobs
     uuid : str
@@ -112,7 +108,9 @@ class ChildJob(BaseSchema):
 
     id = Column(Integer, primary_key=True, nullable=False)
     uuid = Column(String, unique=True, nullable=False)
-    parent_uuid = Column(String, ForeignKey("parent_jobs.uuid"), primary_key=True, nullable=False)
+    parent_uuid = Column(
+        String, ForeignKey("parent_jobs.uuid"), primary_key=True, nullable=False
+    )
     start = Column(Integer, default=func.now())
     duration = Column(Integer)
     status = Column(String, nullable=False)
@@ -125,6 +123,7 @@ class ChildJob(BaseSchema):
     training_losses = relationship("TrainingLosses", backref="child_job")
     current_checkpoint = Column(LargeBinary)
     optimal_checkpoint = Column(LargeBinary)
+
 
 class SequenceEmbeddings(BaseSchema):
     """
@@ -148,12 +147,15 @@ class SequenceEmbeddings(BaseSchema):
 
     __tablename__ = "sequence_embeddings"
 
-    child_uuid = Column(String, ForeignKey("child_jobs.uuid"), primary_key=True, nullable=False)
+    child_uuid = Column(
+        String, ForeignKey("child_jobs.uuid"), primary_key=True, nullable=False
+    )
     seq_id = Column(Integer, primary_key=True, nullable=False)
     random_region = Column(String)
     coord_x = Column(Float)
     coord_y = Column(Float)
     duplicate = Column(Integer)
+
 
 class TrainingLosses(BaseSchema):
     """
@@ -177,7 +179,9 @@ class TrainingLosses(BaseSchema):
 
     __tablename__ = "training_losses"
 
-    child_uuid = Column(String, ForeignKey("child_jobs.uuid"), primary_key=True, nullable=False)
+    child_uuid = Column(
+        String, ForeignKey("child_jobs.uuid"), primary_key=True, nullable=False
+    )
     epoch = Column(Integer, primary_key=True, nullable=False)
 
     train_loss = Column(Float)
@@ -186,192 +190,295 @@ class TrainingLosses(BaseSchema):
     test_kld = Column(Float)
 
 
-# implements accessors for the database
-def read_jobs_data(
-    table: Literal["parent_jobs", "child_jobs", "sequence_embeddings", "training_losses"],
-    uuid: Optional[str] = None,
-):
+class JobDatabaseService:
     """
-    Read the data from the database
+    JobDatabaseService
+    ==================
 
-    if the database is not consistent with the redis database, update the database
-    if query is not found, return None
+    Handles the database for the training jobs.
 
-    Parameters
+    Attributes
     ----------
-    table : str
-        table name to read the data from
-    uuid : str
-        identifier of the job, either parent or child job
-        when not provided, return all the data from the table
+    session: sqlalchemy.orm.session.Session
+        if not provided, create a new session
+
+    Methods
+    -------
+    read_jobs_data(table: str, uuid: Optional[str] = None)
+        Read the data from the database
+
     """
 
-    if table == "parent_jobs":
-        # read data
-        if uuid is None:
-            data = session.query(ParentJob).all()
-        else:
-            data = session.query(ParentJob).filter(ParentJob.uuid == uuid).all()
-        session.commit()
-        
-        # if any modification is found, update the database
-        for d in data:
-            res = celery.AsyncResult(d.uuid)
-            if res is None:
-                continue # skip if the job is not found
-            status = str(res.status)
-            if status != d.status:
-                d.status = status
-        session.commit()
-    
-    elif table == "child_jobs":
-        if uuid is None:
-            data = session.query(ChildJob).all()
-        else:
-            data = session.query(ChildJob).filter(ChildJob.uuid == uuid).all()
-        session.commit()
-        # child jobs does not use redis, so no need to update the database
+    def __init__(self, session: Optional[Session] = None):
+        if session is None:
+            engine = create_engine("postgresql://postgres:postgres@db:5432")
+            session = sessionmaker(bind=engine)()
+        self.session = session
 
-    elif table == "sequence_embeddings":
-        if uuid is None:
-            data = session.query(SequenceEmbeddings).all()
-        else:
-            data = session.query(SequenceEmbeddings).filter(SequenceEmbeddings.child_uuid == uuid).all()
-        session.commit()
+    # implements accessors for the database
+    def get_data(
+        self,
+        table: Literal[
+            "parent_jobs", "child_jobs", "sequence_embeddings", "training_losses"
+        ],
+        uuid: Optional[str] = None,
+    ):
+        """
+        Read the data from the database
 
-    elif table == "training_losses":
-        if uuid is None:
-            data = session.query(TrainingLosses).all()
-        else:
-            data = session.query(TrainingLosses).filter(TrainingLosses.child_uuid == uuid).all()
-        session.commit()
+        if the database is not consistent with the redis database, update the database
+        if query is not found, return None
 
-    else:
-        raise ValueError("Table name is not valid")
+        Parameters
+        ----------
+        table : str
+            table name to read the data from
+        uuid : str
+            identifier of the job, either parent or child job
+            when not provided, return all the data from the table
+        """
 
-    return data
-
-
-def commit_jobs_data(
-    table: Literal["parent_jobs", "child_jobs", "sequence_embeddings", "training_losses"],
-    method: Literal["add", "update", "delete"],
-    uuid: str,
-    data: Optional[Dict[str, Any]] = None,
-) -> None:
-    """
-    Commit the data to the database
-
-    Parameters
-    ----------
-    table: str
-        table name to commit the data to
-    method: str
-        method to commit the data, either "add", "update", or "delete"
-    uuid: str
-        identifier of the job, either parent or child job
-    data: dict
-        value to commit to the table
-    """
-
-    if method == "add":
-        if data is None:
-            raise ValueError("Data must be provided for add method")
-        
         if table == "parent_jobs":
-            parent_job = ParentJob(**data)
-            session.add(parent_job)
-        elif table == "child_jobs":
-            child_job = ChildJob(**data)
-            session.add(child_job)
-        elif table == "sequence_embeddings":
-            sequence_embedding = SequenceEmbeddings(**data)
-            session.add(sequence_embedding)
-        elif table == "training_losses":
-            training_loss = TrainingLosses(**data)
-            session.add(training_loss)
-        else:
-            raise ValueError("Table name is not valid")
-        
-    elif method == "update":
-        if data is None:
-            raise ValueError("Data must be provided for update method")
-        if table == "parent_jobs":
-            data = ParentJob(data)
-            session.query(ParentJob).filter(ParentJob.uuid == uuid).update(data)
-        elif table == "child_jobs":
-            data = ChildJob(data)
-            session.query(ChildJob).filter(ChildJob.uuid == uuid).update(data)
-        elif table == "sequence_embeddings":
-            data = SequenceEmbeddings(data)
-            session.query(SequenceEmbeddings).filter(SequenceEmbeddings.child_uuid == uuid).update(data)
-        elif table == "training_losses":
-            data = TrainingLosses(data)
-            session.query(TrainingLosses).filter(TrainingLosses.child_uuid == uuid).update(data)
-        else:
-            raise ValueError("Table name is not valid")
-        
-    elif method == "delete":
-        if table == "parent_jobs":
-            session.query(ParentJob).filter(ParentJob.uuid == uuid).delete()
-        elif table == "child_jobs":
-            session.query(ChildJob).filter(ChildJob.uuid == uuid).delete()
-        elif table == "sequence_embeddings":
-            session.query(SequenceEmbeddings).filter(SequenceEmbeddings.child_uuid == uuid).delete()
-        elif table == "training_losses":
-            session.query(TrainingLosses).filter(TrainingLosses.child_uuid == uuid).delete()
-        else:
-            raise ValueError("Table name is not valid")
+            # read data
+            if uuid is None:
+                data = self.session.query(ParentJob).all()
+            else:
+                data = (
+                    self.session.query(ParentJob).filter(ParentJob.uuid == uuid).all()
+                )
+            self.session.commit()
 
-    session.commit()
-
-    return
-
-    
-def commit_jobs_batch_data(
-    table: Literal["child_jobs", "sequence_embeddings", "training_losses"],
-    method: Literal["add", "delete"],
-    uuid: str,
-    data: Optional[List[Dict[str, Any]]] = None,
-) -> None:
-    """
-    Commit the data to the database
-
-    Parameters
-    ----------
-    table : str
-        table name to commit the data to
-    method: str
-        method to commit the data, either "add", "update", or "delete"
-    uuid : str
-        identifier of the job, either parent or child job
-    data : list
-        list of data to commit to the table
-    """
-
-    if method == "add":
-        if data == [] or data is None:
-            raise ValueError("Data must be provided for add method")
-        if table == "sequence_embeddings":
+            # if any modification is found, update the database
             for d in data:
-                sequence_embedding = SequenceEmbeddings(**d)
-                session.add(sequence_embedding)
-        elif table == "training_losses":
-            raise NotImplementedError("Batch add is not supported for training losses")
-        elif table == "child_jobs":
-            raise NotImplementedError("Batch add is not supported for child jobs")
-        else:
-            raise ValueError("Table name is not valid")
-        
-    elif method == "delete":
-        if table == "sequence_embeddings":
-            session.query(SequenceEmbeddings).filter(SequenceEmbeddings.child_uuid == uuid).delete()
-        elif table == "training_losses":
-            session.query(TrainingLosses).filter(TrainingLosses.child_uuid == uuid).delete()
-        elif table == "child_jobs":
-            session.query(ChildJob).filter(ChildJob.parent_uuid == uuid).delete()
-        else:
-            raise ValueError("Table name is not valid")
-    
-    session.commit()
+                res = celery.AsyncResult(d.uuid)
+                if res is None:
+                    continue  # skip if the job is not found
+                redis_status = str(res.status)
+                # print(redis_status)
+                allowed_pairs = {
+                    "PENDING": ["pending"],
+                    "STARTED": ["progress"],
+                    "SUCCESS": ["success", "suspend"],
+                    "FAILURE": ["failure"],
+                    "RETRY": ["pending"],
+                }
+                if d.status not in allowed_pairs[redis_status]:
+                    d.status = allowed_pairs[redis_status][0]
 
+            self.session.commit()
+
+        elif table == "child_jobs":
+            if uuid is None:
+                data = self.session.query(ChildJob).all()
+            else:
+                data = self.session.query(ChildJob).filter_by(uuid=uuid).all()
+                print(data)
+            self.session.commit()
+            # child jobs does not use redis, so no need to update the database
+
+        elif table == "sequence_embeddings":
+            if uuid is None:
+                data = self.session.query(SequenceEmbeddings).all()
+            else:
+                data = (
+                    self.session.query(SequenceEmbeddings)
+                    .filter(SequenceEmbeddings.child_uuid == uuid)
+                    .all()
+                )
+            self.session.commit()
+
+        elif table == "training_losses":
+            if uuid is None:
+                data = self.session.query(TrainingLosses).all()
+            else:
+                data = (
+                    self.session.query(TrainingLosses)
+                    .filter(TrainingLosses.child_uuid == uuid)
+                    .all()
+                )
+            self.session.commit()
+
+        else:
+            raise ValueError("Table name is not valid")
+
+        return data
+
+    def initialize_job(
+        self,
+        parent: ParentJob,
+        children: List[ChildJob],
+    ):
+        """
+        Add the parent job to the database
+
+        Parameters
+        ----------
+        parent : ParentJob
+            parent job data to add to the database
+        children : List[ChildJob]
+        """
+        self.session.add(parent)
+        self.session.add_all(children)
+        self.session.commit()
+        return
+
+    def update_parent_job(
+        self,
+        parent_uuid: str,
+        patch: Dict[str, Any],
+    ) -> None:
+        """
+        Update the parent job in the database
+
+        Parameters
+        ----------
+        parent_uuid : str
+            identifier of the parent job
+        data : dict
+            data to update the parent job
+        """
+        self.session.query(ParentJob).filter_by(uuid=parent_uuid).update(patch)
+        self.session.commit()
+        return
+
+    def update_child_job(
+        self,
+        child_uuid: str,
+        patch: Dict[str, Any],
+    ) -> None:
+        """
+        Update the child job in the database
+
+        Parameters
+        ----------
+        child_uuid : str
+            identifier of the child job
+        data : dict
+            data to update the child job
+        """
+        self.session.query(ChildJob).filter(ChildJob.uuid == child_uuid).update(patch)
+        self.session.commit()
+        return
+
+    def update_sequence_embeddings(
+        self,
+    ) -> None:
+        """
+        Update the sequence embeddings in the database
+
+        Parameters
+        ----------
+        child_uuid : str
+            identifier of the child job
+        data : dict
+            data to update the sequence embeddings
+        """
+        assert new_latents.shape[1] == 2, "The shape of the new latents must be (n, 2)"
+        self.session.query(SequenceEmbeddings).filter(
+            SequenceEmbeddings.child_uuid == child_uuid
+        ).delete()
+        self.session.add_all(
+            [
+                SequenceEmbeddings(
+                    child_uuid=child_uuid,
+                    seq_id=i,
+                    coord_x=x,
+                    coord_y=y,
+                )
+                for i, (x, y) in enumerate(new_latents)
+            ]
+        )
+        self.session.add_all([SequenceEmbeddings(**d) for d in new_latents])
+        self.session.commit()
+        return
+
+    def add_training_losses(
+        self,
+        child_uuid: str,
+        epoch: int,
+        train_loss: float,
+        test_loss: float,
+        test_recon: float,
+        test_kld: float,
+    ):
+        """
+        Add the training losses to the database
+
+        Parameters
+        ----------
+        child_uuid : str
+            identifier of the child job
+        epoch : int
+            epoch of the training losses
+        train_loss : float
+            training loss of the child job
+        test_loss : float
+            test loss of the child job
+        test_recon : float
+            test reconstruction loss of the child job
+        test_kld : float
+            test Kullback-Leibler divergence (KLD) of the child job
+        """
+        self.session.add(
+            TrainingLosses(
+                child_uuid=child_uuid,
+                epoch=epoch,
+                train_loss=train_loss,
+                test_loss=test_loss,
+                test_recon=test_recon,
+                test_kld=test_kld,
+            )
+        )
+        self.session.commit()
+        return
+
+    def delete_job(
+        self,
+        parent_uuid: str,
+    ):
+        """
+        Delete the parent job from the database
+
+        Parameters
+        ----------
+        parent_uuid : str
+            identifier of the parent job
+        """
+        self.session.query(ParentJob).filter(ParentJob.uuid == parent_uuid).delete()
+        child_jobs = (
+            self.session.query(ChildJob)
+            .filter(ChildJob.parent_uuid == parent_uuid)
+            .all()
+        )
+        for child_job in child_jobs:
+            self.session.query(SequenceEmbeddings).filter(
+                SequenceEmbeddings.child_uuid == child_job.uuid
+            ).delete()
+            self.session.query(TrainingLosses).filter(
+                TrainingLosses.child_uuid == child_job.uuid
+            ).delete()
+            self.session.query(ChildJob).filter(
+                ChildJob.uuid == child_job.uuid
+            ).delete()
+        self.session.commit()
+        return
+
+
+async def get_db_handler():
+    """
+    Get the database handler
+    """
+    engine = create_engine("postgresql://postgres:postgres@db:5432")
+    session = sessionmaker(bind=engine)()
+    db = JobDatabaseService(session)
+    BaseSchema.metadata.create_all(engine)
+    try:
+        yield db
+        db.session.commit()
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        raise e
+    finally:
+        db.session.close()
+        engine.dispose()
     return
