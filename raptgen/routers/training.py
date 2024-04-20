1ef4cd7393ac95a01bbb51b8fc103b2b4317a421
Ryota Nakano <71750787+unkosan@users.noreply.github.com>
2024-04-20 19:31:13 +0900

    feat: replaced sqlite to postgres

--- raptgen/routers/training.py
+++ raptgen/routers/training.py
@@ -1,41 +1,39 @@
 import torch
-from fastapi import APIRouter, File, Form, Body, HTTPException
-from pydantic import BaseModel, Field
-from typing import List, Dict, Any, Optional
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
-
-from core.db import (
-    get_engine,
-    get_session,
-    ChildJob,
-    ParentJob,
-    SequenceEmbeddings,
-    TrainingLosses,
-)
-from fastapi import Depends
 import re
 
+from typing import List, Any, Optional
 
-def get_db_session():
-    """Dependency to get the database session"""
-    session = get_session()
-    try:
-        yield session
-    finally:
-        session.close()
 
+from core.db import (
+    ParentJob,
+    ChildJob,
+    SequenceEmbeddings,
+    TrainingLosses,
+    get_db_session,
+)
 
 router = APIRouter()
 
 
 @router.get("/api/train/device/process")
 async def get_available_devices():
-    """get available devices"""
-    devices = ["CPU"]
+    """
+    Get available devices for training.
+
+    Returns
+    -------
+    devices : List[str]
+        list of available devices
+    """
+
+    devices = ["CPU"]  # default device
 
     if torch.cuda.is_available():
         cuda_device_count = torch.cuda.device_count()
-        devices.extend([f"CUDA:{i}" for i in range(cuda_device_count)])
+        devices += [f"CUDA:{i}" for i in range(cuda_device_count)]
 
     return devices
 
@@ -43,7 +41,7 @@ async def get_available_devices():
 @router.get("/api/train/jobs/items/{parent_uuid}")
 async def get_parent_job(
     parent_uuid: str,
-    session: Session = Depends(get_db_session),  # Use dependency injection for session
+    session: Session = Depends(get_db_session),
 ):
     """
     Retrieve detailed information about a specific parent job based on its UUID.
@@ -78,10 +76,10 @@ async def get_parent_job(
     -----
     The summary includes indices, statuses, epochs finished, and minimum NLLs of all child jobs.
     """
-    job = session.query(ParentJob).filter(ParentJob.uuid == parent_uuid).first()
-    if job is None:
+    parent_job = session.query(ParentJob).filter(ParentJob.uuid == parent_uuid).first()
+    if parent_job is None:
         raise HTTPException(
-            status_code=422,
+            status_code=422,  # TODO: 404 is more suited, but 422 is used for consistency
             detail=[
                 {
                     "loc": ["body", "search_regex"],
@@ -97,21 +95,21 @@ async def get_parent_job(
         "epochs_finished": [],
         "minimum_NLLs": [],
     }
-    for child_job in job.child_jobs:
+    for child_job in parent_job.child_jobs:
         summary["indices"].append(child_job.id)
         summary["statuses"].append(child_job.status)
         summary["epochs_finished"].append(child_job.epochs_current)
         summary["minimum_NLLs"].append(child_job.minimum_NLL)
 
     response = {
-        "uuid": job.uuid,
-        "name": job.name,
-        "type": job.type,
-        "status": job.status,
-        "start": job.start,
-        "duration": job.duration,
-        "reiteration": job.reiteration,
-        "params_training": job.params_training,
+        "uuid": parent_job.uuid,
+        "name": parent_job.name,
+        "type": parent_job.type,
+        "status": parent_job.status,
+        "start": parent_job.start,
+        "duration": parent_job.duration,
+        "reiteration": parent_job.reiteration,
+        "params_training": parent_job.params_training,
         "summary": summary,
     }
     return response
@@ -121,44 +119,42 @@ async def get_parent_job(
 async def get_child_job(
     parent_uuid: str,
     child_id: int,
-    session: Session = Depends(get_db_session),  # Use dependency injection for session
+    session: Session = Depends(get_db_session),
 ):
     """
-    Retrieve detailed information about a specific child job based on its parent UUID and child ID.
+    Retrieve detailed information about a specific parent job based on its UUID.
 
     Parameters
     ----------
     parent_uuid : str
         The UUID of the parent job.
-    child_id : int
-        The ID of the child job.
     session : Session, optional
         The database session, by default uses dependency injection to get the session.
 
     Returns
     -------
     dict
-        A dictionary containing detailed information about the child job. This includes:
-        - uuid: The UUID of the child job.
-        - id: The ID of the child job.
-        - status: The status of the child job.
-        - start: The start time of the child job.
-        - is_added_viewer_dataset: Boolean indicating if the viewer dataset was added.
-        - error_msg: Error message if the status is 'failure'.
-        - latent: Dictionary containing embedding details if the status is 'success', 'progress', or 'suspend'.
-        - losses: Dictionary containing training loss details if the status is 'success', 'progress', or 'suspend'.
-        - epochs_current: the number of finished epochs of the child job. (that is, zero-indexed value)
-        - epochs_total: Total epochs of the child job.
+        A dictionary containing detailed information about the parent job. This includes:
+        - uuid: The UUID of the parent job.
+        - name: The name of the parent job.
+        - type: The type of the parent job.
+        - status: The status of the parent job.
+        - start: The start time of the parent job.
+        - duration: The duration of the parent job.
+        - reiteration: The reiteration count of the parent job.
+        - params_training: The training parameters of the parent job.
+        - summary: A summary of all child jobs associated with the parent job.
 
     Raises
     ------
     HTTPException
-        If the child job is not found in the database.
+        If the parent job is not found in the database.
 
     Notes
     -----
-    TODO: Consider changing the status code 422 to 404 for "Child Job not found".
+    The summary includes indices, statuses, epochs finished, and minimum NLLs of all child jobs.
     """
+
     child_job = (
         session.query(ChildJob)
         .filter(ChildJob.parent_uuid == parent_uuid, ChildJob.id == child_id)
@@ -169,6 +165,7 @@ async def get_child_job(
             status_code=422, detail="Child Job not found"
         )  # TODO: 404 is more appropriate than 422
     # Assuming similar fields to the parent job
+
     response = {
         "uuid": child_job.uuid,
         "id": child_job.id,
@@ -180,7 +177,7 @@ async def get_child_job(
 
     if child_job.status in {"suspend", "progress"}:
         response["epochs_current"] = child_job.epochs_current
-    if child_job.status == "failure":
+    if child_job.status in {"failure"}:
         response["error_msg"] = child_job.error_msg
     if child_job.status in {"success", "progress", "suspend"}:
         embeddings = (
@@ -253,34 +250,48 @@ class SearchPayload(BaseModel):
 @router.post("/api/train/jobs/search")
 async def search_jobs(
     request: SearchPayload,
-    session: Session = Depends(get_db_session),  # Use dependency injection for session
+    session: Session = Depends(get_db_session),
 ):
+    """
+    Search for jobs based on the given parameters.
+
+    Parameters
+    ----------
+    request : SearchPayload
+        The search parameters.
+        status: List of statuses to filter the jobs.
+        search_regex: Regular expression to filter the jobs.
+        is_multiple: Boolean to filter reiteration > 2.
+        type: List of types (RaptGen, RaptGen-freq, etc.) to filter the jobs.
+    session : Session, optional
+        The database session, by default uses dependency injection to get the session.
+    """
+
     query = session.query(ParentJob)
 
-    # if 'status' field is provided in the request
+    # Filter by status
     if request.status is not None:
         query = query.filter(ParentJob.status.in_(request.status))
 
-    # if 'search_regex' field is provided in the request
+    # Filter by search_regex
     if request.search_regex is not None:
         try:
-            # check if the regex pattern is valid
             re.compile(request.search_regex)
-            query = query.filter(ParentJob.name.op("REGEXP")(request.search_regex))
+            query = query.filter(ParentJob.name.op("~")(request.search_regex))
         except re.error:
             raise HTTPException(
                 status_code=422,
                 detail=[
                     {
                         "loc": ["body", "search_regex"],
-                        "msg": f"Invalid regex pattern: {request.search_regex}",
+                        "msg": "Invalid regular expression",
                         "type": "value_error",
                     }
                 ],
             )
 
-    # if 'type' field is provided in the request
-    if request.type is not None:
+    # Filter by type
+    if request.type:
         if not set(request.type).issubset(
             {"RaptGen", "RaptGen-freq", "RaptGen-logfreq"}
         ):
@@ -296,9 +307,9 @@ async def search_jobs(
             )
         query = query.filter(ParentJob.type.in_(request.type))
 
-    # if 'is_multiple' field is provided in the request, we assume that this refers to whether a Job has multiple ChildJobs
+    # Filter by is_multiple
     if request.is_multiple is not None:
-        if request.is_multiple is True:
+        if request.is_multiple:
             query = query.filter(ParentJob.reiteration > 1)
         else:
             query = query.filter(ParentJob.reiteration == 1)
@@ -345,7 +356,7 @@ async def update_parent_job(
     parent_uuid: str,
     request: UpdateParentJobPayload,
     session: Session = Depends(get_db_session),  # Use dependency injection for session
-) -> dict:
+):
     """
     Update the property of a parent job based on its UUID.
 
@@ -363,8 +374,8 @@ async def update_parent_job(
     None
         The response is an empty dictionary.
     """
-    job = session.query(ParentJob).filter(ParentJob.uuid == parent_uuid).first()
-    if job is None:
+    parent_job = session.query(ParentJob).filter(ParentJob.uuid == parent_uuid).first()
+    if parent_job is None:
         raise HTTPException(
             status_code=422,  # TODO: 404 is more appropriate than 422, but it's not consistent
             detail=[
@@ -378,7 +389,7 @@ async def update_parent_job(
 
     if request.target not in {"name"}:
         raise HTTPException(
-            status_code=422,  # TODO: 404 is more appropriate than 422, but it's not consistent
+            status_code=422,
             detail=[
                 {
                     "loc": ["body", "target"],
@@ -389,9 +400,13 @@ async def update_parent_job(
         )
 
     if request.target == "name":
-        if not isinstance(request.value, str) or len(request.value.strip()) == 0:
+        if isinstance(request.value, str) and len(request.value) > 0:
+            setattr(parent_job, request.target, request.value)
+            session.commit()
+            return {}
+        else:
             raise HTTPException(
-                status_code=422,  # TODO: 404 is more appropriate than 422, but it's not consistent
+                status_code=422,
                 detail=[
                     {
                         "loc": ["body", "value"],
@@ -400,10 +415,6 @@ async def update_parent_job(
                     }
                 ],
             )
-        else:
-            setattr(job, request.target, request.value)
-            session.commit()
-            return {}
 
     # for future targets, raise an exception
     raise Exception(f"Not implemented target {request.target}")
