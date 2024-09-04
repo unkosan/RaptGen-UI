feat: Fix child job minimum NLL calculation

raptgen/routers/training.py
@@ -119,7 +119,8 @@ async def get_parent_job(
         summary["indices"].append(child_job.id)
         summary["statuses"].append(child_job.status)
         summary["epochs_finished"].append(child_job.epochs_current)
-        if np.isnan(child_job.minimum_NLL):
+
+        if child_job.minimum_NLL is not None and not np.isnan(child_job.minimum_NLL):
             summary["minimum_NLLs"].append(child_job.minimum_NLL)
         else:
             summary["minimum_NLLs"].append(None)
@@ -879,7 +880,9 @@ async def publish_parent_job(
         inplace=True,
     )
 
-    model_binary: bytes = session.query(ChildJob).filter(ChildJob.uuid == uuid).first().optimal_checkpoint  # type: ignore
+    model_binary: bytes = (
+        session.query(ChildJob).filter(ChildJob.uuid == uuid).first().optimal_checkpoint
+    )  # type: ignore
     state_dict_map = torch.load(BytesIO(model_binary))
     state_dict = state_dict_map["model"]
 