003dca07e184cda85321fba916eadcbdd973c4ac
Ryota Nakano <71750787+unkosan@users.noreply.github.com>
2024-10-10 20:10:30 +0900

    refactor: modefined zodios for renewed backend API

--- visualizer/services/route/session.ts
+++ visualizer/services/route/session.ts
@@ -3,87 +3,45 @@ import { z } from "zod";
 
 // API GET /session/start
 export const requestGetStartSession = z.void();
-export const responseGetStartSession = z.union([
-  z.object({
-    status: z.enum(["success"]),
-    data: z.number(),
-  }),
-  z.object({
-    status: z.enum(["error"]),
-  }),
-]);
+export const responseGetStartSession = z.object({
+  uuid: z.string().uuid(),
+});
 
 // API GET /session/end
 export const requestGetEndSession = z.void();
-export const responseGetEndSession = z.object({
-  status: z.enum(["success", "error"]),
-});
+export const responseGetEndSession = z.null();
 
 // API GET /session/status
 export const requestGetSessionStatus = z.void();
-export const responseGetSessionStatus = z.union([
-  z.object({
-    status: z.enum(["success"]),
-    data: z.array(z.number()),
-  }),
-  z.object({
-    status: z.enum(["error"]),
-  }),
-]);
+export const responseGetSessionStatus = z.object({
+  entries: z.array(z.string().uuid()),
+});
 
 // API POST /session/encode
 export const requestPostEncode = z.object({
-  session_id: z.number().int(),
+  session_uuid: z.string().uuid(),
   sequences: z.array(z.string().regex(/^[AUTCG]+$/i)).nonempty(),
 });
-export const responsePostEncode = z.union([
-  z.object({
-    status: z.enum(["success"]),
-    data: z.array(
-      z.object({
-        coord_x: z.number(),
-        coord_y: z.number(),
-      })
-    ),
-  }),
-  z.object({
-    status: z.enum(["error"]),
-  }),
-]);
+export const responsePostEncode = z.object({
+  coords_x: z.array(z.number()),
+  coords_y: z.array(z.number()),
+});
 
 // API POST /session/decode
 export const requestPostDecode = z.object({
-  session_id: z.number().int(),
-  coords: z
-    .array(
-      z.object({
-        coord_x: z.number(),
-        coord_y: z.number(),
-      })
-    )
-    .nonempty(),
+  session_uuid: z.string().uuid(),
+  coords_x: z.array(z.number()).nonempty(),
+  coords_y: z.array(z.number()).nonempty(),
 });
-export const responsePostDecode = z.union([
-  z.object({
-    status: z.enum(["success"]),
-    data: z.array(z.string()),
-  }),
-  z.object({
-    status: z.enum(["error"]),
-  }),
-]);
+export const responsePostDecode = z.object({
+  sequences: z.array(z.string()).nonempty(),
+});
 
 // API POST /session/decode/weblogo (return image)
 export const requestPostWeblogo = z.object({
-  session_id: z.number().int(),
-  coords: z
-    .array(
-      z.object({
-        coord_x: z.number(),
-        coord_y: z.number(),
-      })
-    )
-    .nonempty(),
+  session_uuid: z.string().uuid(),
+  coords_x: z.array(z.number()).length(1),
+  coords_y: z.array(z.number()).length(1),
 });
 export const responsePostWeblogo = z.string().nonempty();
 
@@ -95,10 +53,10 @@ export const apiSession = makeApi([
     description: "Start session",
     parameters: [
       {
-        name: "VAE_name",
-        description: "VAE model name",
+        name: "vae_uuid",
+        description: "UUID of the VAE model",
         type: "Query",
-        schema: z.string().nonempty(),
+        schema: z.string().uuid(),
       },
     ],
     response: responseGetStartSession,
@@ -110,10 +68,10 @@ export const apiSession = makeApi([
     description: "End session",
     parameters: [
       {
-        name: "session_id",
+        name: "session_uuid",
         description: "Session ID",
         type: "Query",
-        schema: z.number().int(),
+        schema: z.string().uuid(),
       },
     ],
     response: responseGetEndSession,
