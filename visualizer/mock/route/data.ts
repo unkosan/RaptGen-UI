339df81d42655e70ad39acd9c4157c9ef02b9f3d
Ryota Nakano <71750787+unkosan@users.noreply.github.com>
2024-10-11 15:05:10 +0900

    refactor: modified mocks for frontend

--- visualizer/mock/route/data.ts
+++ visualizer/mock/route/data.ts
@@ -1,10 +1,8 @@
 import { rest } from "msw";
-import * as encodeZod from "~/services/api-client";
 import { vaeParams, gmmParams } from "./asset/params";
 import { selex } from "./asset/rapt-selex";
-import { selex as randSelex } from "./asset/random-selex";
 import { gmm } from "./asset/gmm-values";
-import { measuredData } from "./asset/measured-values";
+import { uuids } from "./asset/uuids";
 
 export const mockURL = (path: string) => {
   return `http://localhost:3000/api${path}`;
@@ -23,203 +21,176 @@ export const dataHandlers = [
 
   rest.get(mockURL("/data/VAE-model-names"), (req, res, ctx) => {
     return res(
-      ctx.status(200),
       ctx.json({
-        status: "success",
-        data: ["RAPT1", "RAPT3"],
+        entries: [
+          { uuid: uuids.vae.rapt1, name: "RAPT1" },
+          { uuid: uuids.vae.rapt3, name: "RAPT3" },
+        ],
       })
     );
   }),
 
   rest.get(mockURL("/data/GMM-model-names"), (req, res, ctx) => {
-    const VAE_model_name = req.url.searchParams.get("VAE_model_name");
-    if (VAE_model_name === "RAPT1") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "success",
-          data: ["num_comp_15_A"],
-        })
-      );
-    } else if (VAE_model_name === "RAPT3") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "success",
-          data: ["num_comp_15_B"],
-        })
-      );
-    } else {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "error",
-          data: ["none"],
-        })
-      );
+    const vae_uuid = req.url.searchParams.get("vae_uuid");
+    switch (vae_uuid) {
+      case uuids.vae.rapt1:
+        return res(
+          ctx.status(200),
+          ctx.json({
+            entries: [{ uuid: uuids.gmm.rapt1, name: "num_comp_15_A" }],
+          })
+        );
+      case uuids.vae.rapt3:
+        return res(
+          ctx.status(200),
+          ctx.json({
+            entries: [{ uuid: uuids.gmm.rapt3, name: "num_comp_15_B" }],
+          })
+        );
+      default:
+        return res(
+          ctx.status(404),
+          ctx.json({
+            error: "error",
+          })
+        );
     }
   }),
 
-  rest.get(mockURL("/data/measured-data-names"), (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        status: "success",
-        data: ["report1.csv", "report3.csv"],
-      })
-    );
-  }),
+  // not available in the current implementation
+  // rest.get(mockURL("/data/measured-data-names"), (req, res, ctx) => {
+  //   return res(
+  //     ctx.status(200),
+  //     ctx.json({
+  //       status: "success",
+  //       data: ["report1.csv", "report3.csv"],
+  //     })
+  //   );
+  // }),
 
   rest.get(mockURL("/data/VAE-model-parameters"), (req, res, ctx) => {
-    const VAE_model_name = req.url.searchParams.get("VAE_model_name");
-    if (VAE_model_name === "RAPT1") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "success",
-          data: vaeParams.RAPT1,
-        })
-      );
-    } else if (VAE_model_name === "RAPT3") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "success",
-          data: vaeParams.RAPT3,
-        })
-      );
-    } else {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "error",
-          data: ["none"],
-        })
-      );
+    const vae_uuid = req.url.searchParams.get("vae_uuid");
+    switch (vae_uuid) {
+      case uuids.vae.rapt1:
+        return res(ctx.status(200), ctx.json(vaeParams.rapt1));
+      case uuids.vae.rapt3:
+        return res(ctx.status(200), ctx.json(vaeParams.rapt3));
+      default:
+        return res(
+          ctx.status(404),
+          ctx.json({
+            error: "error",
+          })
+        );
     }
   }),
 
   rest.get(mockURL("/data/GMM-model-parameters"), (req, res, ctx) => {
-    const VAE_model_name = req.url.searchParams.get("VAE_model_name");
-    const GMM_model_name = req.url.searchParams.get("GMM_model_name");
-    if (VAE_model_name === "RAPT1" && GMM_model_name === "num_comp_15_A") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "success",
-          data: gmmParams.num_comp_15_A,
-        })
-      );
-    } else if (
-      VAE_model_name === "RAPT3" &&
-      GMM_model_name === "num_comp_15_B"
-    ) {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "success",
-          data: gmmParams.num_comp_15_B,
-        })
-      );
-    } else {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "error",
-          data: ["none"],
-        })
-      );
+    const gmm_uuid = req.url.searchParams.get("gmm_uuid");
+    switch (gmm_uuid) {
+      case uuids.gmm.rapt1:
+        return res(ctx.status(200), ctx.json(gmmParams.num_comp_15_A));
+      case uuids.gmm.rapt3:
+        return res(ctx.status(200), ctx.json(gmmParams.num_comp_15_B));
+      default:
+        return res(
+          ctx.status(404),
+          ctx.json({
+            error: "error",
+          })
+        );
     }
   }),
 
   rest.get(mockURL("/data/selex-data"), (req, res, ctx) => {
-    const VAE_model_name = req.url.searchParams.get("VAE_model_name");
-    if (VAE_model_name === "RAPT1") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "success",
-          data: selex.RAPT1,
-        })
-      );
-    } else if (VAE_model_name === "RAPT3") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "success",
-          data: selex.RAPT3,
-        })
-      );
-    } else {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "error",
-          data: ["none"],
-        })
-      );
+    const vae_uuid = req.url.searchParams.get("vae_uuid");
+    switch (vae_uuid) {
+      case uuids.vae.rapt1:
+        return res(
+          ctx.status(200),
+          ctx.json({
+            random_regions: selex.RAPT1.Without_Adapters,
+            duplicates: selex.RAPT1.Duplicates,
+            coord_x: selex.RAPT1.coord_x,
+            coord_y: selex.RAPT1.coord_y,
+          })
+        );
+      case uuids.vae.rapt3:
+        return res(
+          ctx.status(200),
+          ctx.json({
+            random_regions: selex.RAPT3.Without_Adapters,
+            duplicates: selex.RAPT3.Duplicates,
+            coord_x: selex.RAPT3.coord_x,
+            coord_y: selex.RAPT3.coord_y,
+          })
+        );
+      default:
+        return res(
+          ctx.status(404),
+          ctx.json({
+            error: "error",
+          })
+        );
     }
   }),
 
   rest.get(mockURL("/data/GMM-model"), (req, res, ctx) => {
-    const VAE_model_name = req.url.searchParams.get("VAE_model_name");
-    const GMM_model_name = req.url.searchParams.get("GMM_model_name");
-    if (VAE_model_name === "RAPT1" && GMM_model_name === "num_comp_15_A") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "success",
-          data: gmm.num_comp_15_A,
-        })
-      );
-    } else if (
-      VAE_model_name === "RAPT3" &&
-      GMM_model_name === "num_comp_15_B"
-    ) {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "success",
-          data: gmm.num_comp_15_B,
-        })
-      );
-    } else {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "error",
-          data: ["none"],
-        })
-      );
+    const gmm_uuid = req.url.searchParams.get("gmm_uuid");
+    switch (gmm_uuid) {
+      case uuids.gmm.rapt1:
+        return res(
+          ctx.status(200),
+          ctx.json({
+            means: gmm.num_comp_15_A.means,
+            covariances: gmm.num_comp_15_A.covariances,
+          })
+        );
+      case uuids.gmm.rapt3:
+        return res(
+          ctx.status(200),
+          ctx.json({
+            means: gmm.num_comp_15_B.means,
+            covariances: gmm.num_comp_15_B.covariances,
+          })
+        );
+      default:
+        return res(
+          ctx.status(404),
+          ctx.json({
+            error: "error",
+          })
+        );
     }
   }),
 
-  rest.get(mockURL("/data/measured-data"), (req, res, ctx) => {
-    const measured_data_name = req.url.searchParams.get("measured_data_name");
-    if (measured_data_name === "report1.csv") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "success",
-          data: measuredData.report1,
-        })
-      );
-    } else if (measured_data_name === "report3.csv") {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "success",
-          data: measuredData.report3,
-        })
-      );
-    } else {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "error",
-          data: ["none"],
-        })
-      );
-    }
-  }),
+  // not available in the current implementation
+  // rest.get(mockURL("/data/measured-data"), (req, res, ctx) => {
+  //   const measured_data_name = req.url.searchParams.get("measured_data_name");
+  //   if (measured_data_name === "report1.csv") {
+  //     return res(
+  //       ctx.status(200),
+  //       ctx.json({
+  //         status: "success",
+  //         data: measuredData.report1,
+  //       })
+  //     );
+  //   } else if (measured_data_name === "report3.csv") {
+  //     return res(
+  //       ctx.status(200),
+  //       ctx.json({
+  //         status: "success",
+  //         data: measuredData.report3,
+  //       })
+  //     );
+  //   } else {
+  //     return res(
+  //       ctx.status(200),
+  //       ctx.json({
+  //         status: "error",
+  //         data: ["none"],
+  //       })
+  //     );
+  //   }
+  // }),
 ];
