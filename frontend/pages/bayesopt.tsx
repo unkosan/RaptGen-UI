5edfd67c0931961c92ba91753917b578e312baf4
Ryota Nakano <71750787+unkosan@users.noreply.github.com>
2025-03-05 20:31:23 +0900

    refactor: modified path to components

--- frontend/pages/bayesopt.tsx
+++ frontend/pages/bayesopt.tsx
@@ -4,19 +4,19 @@ import { NextPage } from "next";
 import { Provider } from "react-redux";
 import { useState } from "react";
 import { useConfirmNavigation } from "~/components/bayesopt/hooks/use-confirm-navigation";
+import { useSessionInitializer } from "~/components/bayesopt/hooks/use-session-initializer";
 import Head from "next/head";
 import Navigator from "~/components/common/navigator";
 import { Col, Container, Row, SSRProvider, Tab, Tabs } from "react-bootstrap";
 import { store } from "~/components/bayesopt/redux/store";
 import { Footer } from "~/components/common/footer";
-import Sessions from "~/components/bayesopt/sessions/index";
-import VaeSelector from "~/components/bayesopt/vae-selector/index";
-import InitialDataset from "~/components/bayesopt/initial-dataset/index";
-import BayesOptConfig from "~/components/bayesopt/bayes-opt-config/index";
-import LatentGraph from "~/components/bayesopt/latent-graph/index";
-import RegisteredTable from "~/components/bayesopt/registered-table/index";
-import QueryTable from "~/components/bayesopt/query-table/index";
-import { useSessionInitializer } from "~/components/bayesopt/hooks/use-session-initializer";
+import Sessions from "~/components/bayesopt/sessions";
+import VaeSelector from "~/components/bayesopt/vae-selector";
+import InitialDataset from "~/components/bayesopt/initial-dataset";
+import BayesOptConfig from "~/components/bayesopt/bayes-opt-config";
+import LatentGraph from "~/components/bayesopt/latent-graph";
+import RegisteredTable from "~/components/bayesopt/registered-table";
+import QueryTable from "~/components/bayesopt/query-table";
 
 const App: React.FC = () => {
   // Use the hook for navigation confirmation and session cleanup
